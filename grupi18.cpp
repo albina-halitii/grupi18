cc468fe0d2efab9e404ab668ad9ba1a54f060f76
Albina Haliti <albina.haliti4@student.uni-pr.edu>
2025-02-11 12:23:05 +0100

    enum dhe struktura e 1

--- grupi18.cpp
+++ grupi18.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+#define NUMRI_I_STUDENTEVE 4
+
+enum Statusi {
+    I_RREGULLT,
+    ME_KORRESPONDENCE
+};
+
+struct Studenti {
+    int id;
+    string emri;
+    string mbiemri;
+    Statusi statusi;
+
+};
+
 int main() {
     return 0;
 }
