73ba08400f9bd9bd5c87b5251424998c963f1fbc
Albina Haliti <albina.haliti4@student.uni-pr.edu>
2025-02-11 22:31:10 +0100

    funksioni main

--- grupi18.cpp
+++ grupi18.cpp
@@ -9,7 +9,7 @@ enum Statusi {
 };
 
 struct Studenti {
-    int id;
+    int id = 0;
     string emri;
     string mbiemri;
     Statusi statusi;
@@ -20,9 +20,50 @@ struct Studenti {
 void shtoStudent() {
     Studenti student;
     cout << "Vendos ID e studentit: ";
-    cin >> student.id;
+    cin >> student.id; 
+    cin.ignore();
+
+    cout << "Vendos emrin e studentit: ";
+    getline(cin, student.emri);
+
+    cout << "Vendos mbiemrin e sudentit: ";
+    getline(cin, student.mbiemri);
+
+    int status;
+    cout << "Vendos statusin (0 - I rregullt, 1 - Me korrespondence): ";
+    cin >> status;
+    student.status = (status == 0) ? I_RREGULLT : ME_KORRESPONDENCE;
+
+    for(int i = 0; i < NUMRI_STUDENTEVE: i++) {
+        if(s[i].id == 0) {
+            s[i] = student;
+            cout << "Studentiu shtua me sukses!" << endl;
+            return;
+        }
+    }
+    cout << "Grupi eshte i plote, nuk mund te shtohet student i ri!" << endl;
+
 }
 
 int main() {
+    Grupi grupi18 = {{1, "Albina", "Haliti", ME_KORRESPONDENCE}, 
+                    {2, "Agnesa", "Godeni", I_RREGULLT}, 
+                    {3, "Anisa", "Potera", I_RREGULLT}, 
+                    {4, "Albina", "Mehmeti", ME_KORRESPONDENCE}};
+                    
+    cout << "Studentet e grupit:" << endl;
+    grupi18.shfaqStudentin(1);
+
+    cout << endl;
+    grupi18.kerkoStudentin(1);
+
+    cout << endl;
+    grupi18.ndryshoStatusin(1, I_RREGULLT);
+    grupi18.shfaqStudent();
+
+    cout << "\nShto nje student te ri:" << endl;
+    grupi18.shtoStudent();
+    grupi18.shfaqStudent();
+    
     return 0;
 }
